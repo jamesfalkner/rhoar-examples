de0e8dcb3f46fa51cf2aa9fb460c11a7bc864a16
jamesfalkner <schtool@gmail.com>
2018-01-08 16:03:25 -0500

    use inventory

--- catalog-wfswarm/src/main/resources/META-INF/load.sql
+++ catalog-wfswarm/src/main/resources/META-INF/load.sql
@@ -1,8 +1,8 @@
-insert into PRODUCT_CATALOG (itemId, name, description, price) values ('329299', 'Red Fedora', 'Official Red Hat Fedora', 34.99);
-insert into PRODUCT_CATALOG (itemId, name, description, price) values ('329199', 'Forge Laptop Sticker', 'JBoss Community Forge Project Sticker', 8.50);
-insert into PRODUCT_CATALOG (itemId, name, description, price) values ('165613', 'Solid Performance Polo', 'Moisture-wicking, antimicrobial 100% polyester design wicks for life of garment.',17.80);
-insert into PRODUCT_CATALOG (itemId, name, description, price) values ('165614', 'Ogio Caliber Polo', 'Moisture-wicking 100% polyester. Rib-knit collar and cuffs; Ogio jacquard tape inside neck', 28.75);
-insert into PRODUCT_CATALOG (itemId, name, description, price) values ('165954', '16 oz. Vortex Tumbler', 'Double-wall insulated, BPA-free, acrylic cup. Push-on lid with thumb-slide closure.', 6.00);
-insert into PRODUCT_CATALOG (itemId, name, description, price) values ('444434', 'Pebble Smart Watch', 'Smart glasses and smart watches are perhaps two of the most exciting developments in recent years.', 24.00);
-insert into PRODUCT_CATALOG (itemId, name, description, price) values ('444435', 'Oculus Rift', 'The world of gaming has also undergone some very unique and compelling tech advances in recent years. ',106.00 );
-insert into PRODUCT_CATALOG (itemId, name, description, price) values ('444436', 'Lytro Camera', 'Consumers who want to up their photography game are looking at newfangled cameras like the  each image to be.', 44.30);
+insert into PRODUCT_CATALOG (itemId, name, description, price, quantity) values ('329299', 'Red Fedora', 'Official Red Hat Fedora', 34.99, 0);
+insert into PRODUCT_CATALOG (itemId, name, description, price, quantity) values ('329199', 'Forge Laptop Sticker', 'JBoss Community Forge Project Sticker', 8.50, 0);
+insert into PRODUCT_CATALOG (itemId, name, description, price, quantity) values ('165613', 'Solid Performance Polo', 'Moisture-wicking, antimicrobial 100% polyester design wicks for life of garment.',17.80, 0);
+insert into PRODUCT_CATALOG (itemId, name, description, price, quantity) values ('165614', 'Ogio Caliber Polo', 'Moisture-wicking 100% polyester. Rib-knit collar and cuffs; Ogio jacquard tape inside neck', 28.75, 0);
+insert into PRODUCT_CATALOG (itemId, name, description, price, quantity) values ('165954', '16 oz. Vortex Tumbler', 'Double-wall insulated, BPA-free, acrylic cup. Push-on lid with thumb-slide closure.', 6.00, 0);
+insert into PRODUCT_CATALOG (itemId, name, description, price, quantity) values ('444434', 'Pebble Smart Watch', 'Smart glasses and smart watches are perhaps two of the most exciting developments in recent years.', 24.00, 0);
+insert into PRODUCT_CATALOG (itemId, name, description, price, quantity) values ('444435', 'Oculus Rift', 'The world of gaming has also undergone some very unique and compelling tech advances in recent years. ',106.00, 0);
+insert into PRODUCT_CATALOG (itemId, name, description, price, quantity) values ('444436', 'Lytro Camera', 'Consumers who want to up their photography game are looking at newfangled cameras like the  each image to be.', 44.30, 0);
